71a57b7054d1dd8dfbd5854900d6d89259d744d7
zigzackattack <zackbabtkis@gmail.com>
2014-05-01 20:30:47 -0700

    Added ExpansionFile to project

---
+++ src/android/ExpansionStorage.java
@@ -0,0 +1,58 @@
+package com.phonegap.plugins.videoplayer;
+
+import java.io.InputStream;
+import com.android.vending.expansion.zipfile.*;
+import android.content.res.AssetFileDescriptor;
+import android.content.Context;
+import java.io.IOException;
+
+import android.util.Log;
+
+class ExpansionStorage {
+
+  private static final String PROTOCOL = "expansion://";
+  private static final String LOG_TAG  = "RiseMedia";
+  private ZipResourceFile store = null;
+
+  public ExpansionStorage(Context ctx) {
+    try {
+    	Log.d(LOG_TAG, "Loading store...");
+      this.store = APKExpansionSupport.getAPKExpansionZipFile(ctx, 2, 0);
+      Log.d(LOG_TAG, "Loaded store");
+    } catch(IOException e) {
+      Log.d(LOG_TAG, e.getMessage());
+      e.printStackTrace();
+    }
+
+    this.debug();
+  }
+
+  public AssetFileDescriptor load(String file) {
+    String filename = file.replace(PROTOCOL, "");
+    Log.d(LOG_TAG, filename);
+    try {
+			InputStream fis = this.store.getInputStream(filename); 
+			if(fis != null) {
+				Log.d(LOG_TAG, "INPUT STREAM NOT NULL!");
+			}
+		} catch(IOException e) {
+			Log.d(LOG_TAG, e.getMessage());
+		}
+		AssetFileDescriptor fd = this.store.getAssetFileDescriptor(filename);
+    Log.d(LOG_TAG, "Got file descriptor");
+
+    return fd;
+  }
+
+  public void debug() {
+		ZipResourceFile.ZipEntryRO[] zro = this.store.getAllEntries();
+		for(ZipResourceFile.ZipEntryRO entry : zro) {
+			Log.d(LOG_TAG, entry.mFileName);
+		}
+  }
+
+  public static Boolean isExpansionFile(String file) {
+    return file.startsWith(PROTOCOL);
+  }
+
+}
